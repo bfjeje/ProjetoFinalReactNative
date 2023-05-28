764db12c4e56b650e7e97e0eee04ce13ecfcc53e
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-05-28 12:22:27 -0300

    Base project

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
-
 import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+import Home from './Screens/Home';
+import Cadastro from './Screens/Cadastro';
+import Editar from './Screens/Editar';
+
+const App = function () {
+  const Stack = createStackNavigator();
 
   return (
-    <View>
-      <Text> Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Editar" component={Editar} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
